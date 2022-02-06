7347ebf3edee06a103970422cf654af7131343b5
Stanley Araki <77555856+StanleyAraki@users.noreply.github.com>
2022-02-06 01:57:42 -0500

    Debugged Parser.js
    Fixed like...everything

--- parser.js
+++ parser.js
@@ -1,9 +1,12 @@
 let result = [];
 
-document.getElementById('submitButton')
-    .addEventListener('click', getFiles);
+let input = document.querySelector('input');
+let textarea = document.querySelector('textarea');
 
-function get_array(){
+input
+    .addEventListener('change', getFiles); //automatically invoked when file uploaded
+    
+function get_array(){ // function that returns the result array containing Events objects.
     return result;
 }
 
@@ -12,28 +15,30 @@ function getFiles(){
     let files = input.files;
     if (files.length == 0) return;
 
-    let reader = new FileReader();
-
     const file = files[0]
+
+    let reader = new FileReader();
 
     reader.onload = (e) => {
         // try to get the text file
         const file = e.target.result;
-        const lines = file.split(/\r\n|\n/);
+        const lines = file.split(/\r\n|\n/); // split line by line
         for (let i = 0; i < lines.length; i++){
             let line = lines[i];
-            if (line.match("[0-9]{2}(/-.)[0-9](2)") != null){
-                result.push(convertLine(line));
+            //check to see if the line contains a date
+            if (line.match("[0-9]{2}(-)[0-9]{2}(-)[0-9]{4}") != null || line.match("[0-9]{2}(/)[0-9]{2}(/)[0-9]{4}") != null || line.match("[0-9]{2}(.)[0-9]{2}(.)[0-9]{4}") != null){
+                result.push(convertLine(line)); //Converts string into google calendar format and adds to collection of Events objects.
             }
         }
-        // add for loop to check if need to parse
+        for (let n = 0; n < result.length; n++){
+            textarea.value += "start: " + result[n].get_start_string();
+            textarea.value += "\nend: " + result[n].get_end_string() + "\n";
+        }
     };
 
     // incase there is an error
     reader.onerror = (e) => alert(e.target.error.name);
-
-    reader.readAsText(files)
-    return result; // don't know if this has to be below or above this line. don't know what readAsText does. 
+    reader.readAsText(file)
 }
 
 function convertLine(line){
@@ -41,19 +46,20 @@ function convertLine(line){
     let event = new Events();
     // name date or date name
     // assume format of date is month date yaer
-    function set_dates(date_string){
+    function set_dates(date_string){ // function that given a date string, converts it into a uniform format
         let formatted = date_string.replace('/', '.');
         formatted = date_string.replace('-', '.');
-        event.set_year_month_date(moment(formatted, "DD.MM.YYYY").format('YYYY-MM-DD')); // set YYYY-MM-DD format
+        event.set_year_month_date(moment(formatted, "MM.DD.YYYY").format('YYYY-MM-DD')); // set YYYY-MM-DD format
+        return;
     }
-    if (words[0] === "Date:"){
+    if (words[0] === "Date:"){ // parses this way if the format is Date: ____ Event name ____
         // the format is Date: ______ names.....
         set_dates(words[1]);
         let nameArray = words.slice(2);
         let event_name = nameArray.join(' '); // concatenate strings with space in between each word
         event.set_name(event_name);
     }
-    else{
+    else{ // or this way if the foramt is EVent Name ______ Date: ______
         let event_name = '';
         for (let i = 0; i < words.length; i++){
             if (words[i] === "Date:"){
@@ -65,12 +71,6 @@ function convertLine(line){
         }
     }
 
-    // make the formatting the way google calendar wants it
-    
     return event;
     
 }
-
-
-
-// series of helper functions to help me parse 
