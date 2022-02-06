c626d29d9c37cb9ed69c21a8b5dbaf79cac6ff14
Stanley Araki <77555856+StanleyAraki@users.noreply.github.com>
2022-02-05 20:32:54 -0500

    Updated Parser.js
    Added algorithm to convert dates into uniform format. Added mechanism to update Events object, push Events into list, and return the list for use in other parts of the program.

--- Parser.js
+++ Parser.js
@@ -1,12 +1,3 @@
-//file with parsing methods
-
-//main method
-
-//helper method
-
-//parsing method
-
-// handle 
 document.getElementById('input')
     .addEventListener('change', getFiles);
 
@@ -14,6 +5,8 @@ function getFiles(){
     // get all files because we can have option to get multiple files
     let files = input.files;
     if (files.length == 0) return;
+
+    let result = []
 
     let reader = new FileReader();
 
@@ -25,8 +18,8 @@ function getFiles(){
         const lines = lines.split(/\r\n|\n/);
         for (let i = 0; i < lines.length; i++){
             let line = lines[i];
-            if (line.includes("Exam") || line.includes("Midterm")){
-                convertLine(line);
+            if (line.match("[0-9]{2}(/-.)[0-9](2)") != null){
+                result.push(convertLine(line));
             }
         }
         // add for loop to check if need to parse
@@ -36,13 +29,44 @@ function getFiles(){
     reader.onerror = (e) => alert(e.target.error.name);
 
     reader.readAsText(files)
+    return result; // don't know if this has to be below or above this line. don't know what readAsText does. 
 }
 
 function convertLine(line){
-    let words = line.split(" ");
-    for (let i = 0; i < words.length; i++){
-        
+    let words = line.split(" "); // splits strings by space
+    let event = new Events();
+    // name date or date name
+    // assume format of date is month date yaer
+    function set_dates(date_string){
+        let formatted = date_string.replace('/', '.');
+        formatted = date_string.replace('-', '.');
+        event.set_year_month_date(moment(formatted, "DD.MM.YYYY").format('YYYY-MM-DD')); // set YYYY-MM-DD format
     }
+    if (words[0] === "Date:"){
+        // the format is Date: ______ names.....
+        set_dates(words[1]);
+        let nameArray = words.slice(2);
+        let event_name = nameArray.join(' '); // concatenate strings with space in between each word
+        event.set_name(event_name);
+    }
+    else{
+        let event_name = '';
+        for (let i = 0; i < words.length; i++){
+            if (words[i] === "Date:"){
+                event.set_name(event_name);
+                set_dates(words[i+1]);
+                continue;
+            }
+            event_name = event_name === '' ? words[i] + ' ' : event_name + ' ' + words[i];
+        }
+    }
+
+    // make the formatting the way google calendar wants it
+    
+    return event;
+    
 }
+
+
 
 // series of helper functions to help me parse 
